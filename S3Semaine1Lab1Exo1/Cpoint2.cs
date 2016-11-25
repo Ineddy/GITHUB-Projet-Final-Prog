16d830d4cca96b316eb2ca4544c2263f16732f08
Ineddy <comeddy.fontaine@gmail.com>
2016-11-25 09:13:29 -0500

    Création du projet

---
+++ S3Semaine1Lab1Exo1/Cpoint2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3Semaine1Lab1Exo1
+{
+    class Cpoint2
+    {
+        private double x;
+        private double y;
+
+
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+
+
+        public Cpoint2()
+        {
+        }
+
+
+        public Cpoint2(double X, double Y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        public void deplace()
+        {
+            x = x + 2;
+            y = y + 2;
+        }
+
+        /*On aurait pu nommer et utiliser les accesseurs directements abcisse et ordonnee  ??? */
+        public double abscisse() {
+            return x;
+        }
+        public double ordonnee()
+        {
+            return y;
+        }
+    }
+}
---
+++ S3Semaine1Lab1Exo1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3Semaine1Lab1Exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exercice 1*/
+            Cpoints1 monPoint = new Cpoints1(4,8);
+            monPoint.affiche();
+            monPoint.deplace();
+            monPoint.affiche();
+            Console.ReadLine();
+
+
+            /*Exercice 2*/
+            /* Cette partie du main gère l'affichage afin de répondre à la demande de l'exercice 2*/
+
+            Cpoint2 monPoint2 = new Cpoint2(7, 12);
+            Console.WriteLine(monPoint2.abscisse());
+            Console.WriteLine(monPoint2.ordonnee());
+
+            monPoint2.deplace();
+            Console.WriteLine(monPoint2.abscisse());
+            Console.WriteLine(monPoint2.ordonnee());
+            Console.ReadLine();
+
+            /*Exercice 3*/
+
+            Cpoint3 monPoint3 = new Cpoint3(4, 8);
+            monPoint3.affiche();
+            monPoint3.deplace();
+            Cpoint3 monPoint4 = new Cpoint3(4, 8);
+            monPoint4.affiche();
+            Console.ReadLine();
+
+        }
+    }
+}
